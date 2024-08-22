285a1e2ff545c505341784309188334128753a67
angck <angck@foxmail.com>
2024-08-22 09:37:49 +0800

    Updates

---
+++ assets/HelloWorld-382d8def.js
@@ -0,0 +1,1 @@
+import{n as o}from"./index-f440dc6a.js";var s=function(){var e=this,n=e.$createElement,r=e._self._c||n;return r("div",[e._v(e._s(e.msg))])},l=[];const _={name:"HelloWorld",props:{msg:{type:String}}},t={};var c=o(_,s,l,!1,a,null,null,null);function a(e){for(let n in t)this[n]=t[n]}const u=function(){return c.exports}();export{u as default};
